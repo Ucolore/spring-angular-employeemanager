00a525deed6a4c593231f99cf1ce8192c6ccee9a
Joaquim Ucolore <joaquim.ucolore@mozabanco.co.mz>
2021-10-01 16:25:19 +0200

    Heroku property

--- src/main/java/com/surecorp/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/surecorp/employeemanager/EmployeemanagerApplication.java
@@ -1,5 +1,6 @@
 package com.surecorp.employeemanager;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.net.URI;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -16,11 +18,13 @@ public class EmployeemanagerApplication {
         SpringApplication.run(EmployeemanagerApplication.class, args);
     }
 
+    @SneakyThrows
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://" +dbUri.getHost()+":"+dbUri.getPort()));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
